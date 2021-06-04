051cd9720218cc25ae659c409d2f3d77f4e4f968
Minal <patilminalp@gmail.com>
2021-06-05 02:45:50 +0530

    Counting number of head & tail wins

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,28 @@
-echo "Welcome in Flip Coin Simulation"
+echo "Welcome to Flip Coin Simulation"
 
+i=1
+H=0
+T=0
+while [ $i -le 11 ]
+do
 Result=$((RANDOM%2))
-if [[ ${Result} -eq 0 ]]
+if [ ${Result} -eq 0 ]
 then
     echo Heads
-elif [[ ${Result} -eq 1 ]]
+    H=$(($H+1))
+elif [ ${Result} -eq 1 ]
 then
     echo Tails
+    T=$(($T+1))
 fi
+i=$(($i+1))
+done
+
+
+if [ $T -gt $H ]
+then
+        echo "Tail wins $T"
+else
+        echo "Heads wins $H"
+fi
+
